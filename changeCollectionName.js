8149f9f48ec31bbe0a1af2a71ff8baece03b6d32
abdec2 <abdec2@hotmail.com>
2022-05-04 15:11:30 +0300

    change name and description implemented

--- changeCollectionName.js
+++ changeCollectionName.js
@@ -3,12 +3,12 @@ const config = require('./config')
 const addToObject = require('./addToObject')
 const filesList = []
 
-fs.readdirSync(config.inputJsonFolderPath).map(file => {
-    let data = fs.readFileSync(`${config.inputJsonFolderPath}${file}`, {encoding:'utf8', flag:'r'})
+fs.readdirSync(config.outputJsonFolderPath).map(file => {
+    let data = fs.readFileSync(`${config.outputJsonFolderPath}${file}`, {encoding:'utf8', flag:'r'})
     data = JSON.parse(data)
     let name = `${config.CollectionName + " #"}${file.split('.')[0]}`
     data.name = name
-    data.description = config.CollectionName + " Collection"
+    data.description = config.CollectionDesc
     fs.writeFileSync(`${config.outputJsonFolderPath}${file}`, JSON.stringify(data, null, 4))
 })
 
--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
     "imageURI":"ipfs://QmVRvtFg5SP7qafVVkKb7FDqhUAZs82xTH1cnWbLLnqC9L/",
     "extension": ".json",
     "ImageFileExt": "webp",
-    "CollectionName": "Nifty Dogs"
+    "CollectionName": "Nifty Dogs Alpha", 
+    "CollectionDesc": "Our mission is to create an exciting platform to help pair trained service dogs with veterans. Using cryptocurrencies and blockchain technologies, our community will generate funds that can be donated to specific causes that directly impact the need that veterans have for trained service dogs."
 }
