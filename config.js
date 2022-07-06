fbf3eba9d98b6a95665f6765e45cf6b73e16bb9b
abdec2 <abdec2@hotmail.com>
2022-07-06 18:56:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     "inputJsonFolderPath" : "./jsons/",
     "outputJsonFolderPath" : "./output/",
-    "imageURI":"ipfs://QmVRvtFg5SP7qafVVkKb7FDqhUAZs82xTH1cnWbLLnqC9L/",
+    "imageURI":"ipfs://QmeMUbbzjxK6Gr85KSr5D65SgkLnESV5U4RVv6Q2vUn2CU/",
     "extension": ".json",
-    "ImageFileExt": "webp",
+    "ImageFileExt": "png",
     "CollectionName": "Nifty Dogs Alpha", 
     "CollectionDesc": "Our mission is to create an exciting platform to help pair trained service dogs with veterans. Using cryptocurrencies and blockchain technologies, our community will generate funds that can be donated to specific causes that directly impact the need that veterans have for trained service dogs."
 }
