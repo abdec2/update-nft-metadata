f5d44950b57a32fb26edb025192dae0cf43b9611
abdec2 <abdec2@hotmail.com>
2022-11-11 22:34:26 +0300

    work

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     "inputJsonFolderPath" : "./jsons/",
     "outputJsonFolderPath" : "./output/",
-    "imageURI":"ipfs://QmPNCzoM6hq53po8rLG8M64pS3QMUaWN6KxLYuHcVTMDsT/",
+    "imageURI":"ipfs://Qma4ZrzLPcCnNp2juYgiQptdg8DKQ9gXU52csUYeFvMYfq/",
     "extension": ".json",
     "ImageFileExt": "png",
-    "CollectionName": "Nifty Dogs Alpha", 
-    "CollectionDesc": "Our mission is to create an exciting platform to help pair trained service dogs with veterans. Using cryptocurrencies and blockchain technologies, our community will generate funds that can be donated to specific causes that directly impact the need that veterans have for trained service dogs."
+    "CollectionName": "Terra Mater", 
+    "CollectionDesc": ""
 }
--- createMetadata.js
+++ createMetadata.js
@@ -4,15 +4,22 @@ const metadata = []
 const filesList = []
 
 
-fs.readdirSync(config.inputJsonFolderPath).map(file => {
-    filesList.push(file)
-})
+// fs.readdirSync(config.outputJsonFolderPath).map(file => {
+//     filesList.push(file)
+// })
 
-filesList.map(file => {
-    let data = fs.readFileSync(`${config.outputJsonFolderPath}${file}`, {encoding:'utf8', flag:'r'})
+// filesList.map(file => {
+//     let data = fs.readFileSync(`${config.outputJsonFolderPath}${file}`, {encoding:'utf8', flag:'r'})
+//     data = JSON.parse(data)
+//     metadata.push(data)
+// })
+
+fs.readdirSync(config.outputJsonFolderPath).map(file => {
+    let data = fs.readFileSync(`${config.outputJsonFolderPath+file}`, {encoding:'utf8', flag:'r'})
     data = JSON.parse(data)
     metadata.push(data)
 })
-
+// console.log(meta)
+metadata.sort(function(a,b){return a.edition - b.edition})
 fs.writeFileSync(`${config.outputJsonFolderPath}_metadata.json`, JSON.stringify(metadata, null, 4))
 
